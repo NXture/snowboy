ea8909902269887ad9ccf05cf02d0b5500dad6a7
NXture <varunvaru27101999@gmail.com>
2022-01-30 00:48:34 +0530

    added few more sample sets

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:16.04
 
 RUN apt update && apt --yes --force-yes install wget unzip build-essential python python-dev virtualenv portaudio19-dev
-RUN wget https://github.com/NXture/snowboy/archive/master.zip && unzip master.zip
+RUN wget https://github.com/nxture/snowboy/archive/master.zip && unzip master.zip
 
 RUN cd snowboy-master/ && \
     virtualenv -p python2 venv/snowboy && \
@@ -14,4 +14,4 @@ RUN apt -y remove wget unzip build-essential portaudio19-dev && apt -y autoremov
 CMD cd snowboy-master/ && \
     . venv/snowboy/bin/activate && \
     cd examples/Python && \
-    python generate_pmdl.py -r1=model/record1.wav -r2=model/record2.wav -r3=model/record3.wav -r4=model/record4.wav -lang=en -n=model/hotword.pmdl
+    python generate_pmdl.py -r1=model/record1.wav -r2=model/record2.wav -r3=model/record3.wav -r4=model/record4.wav -r5=model/record5.wav -r6=model/record6.wav -r7=model/record7.wav -r8=model/record8.wav -r9=model/record9.wav -r10=model/record10.wav -lang=en -n=model/hotword.pmdl
--- examples/Python/generate_pmdl.py
+++ examples/Python/generate_pmdl.py
@@ -21,6 +21,12 @@ def main():
     parser.add_argument('-r2', '--record2', dest="record2", required=True, help="Record voice 2")
     parser.add_argument('-r3', '--record3', dest="record3", required=True, help="Record voice 3")
     parser.add_argument('-r4', '--record4', dest="record4", required=True, help="Record voice 4")
+    parser.add_argument('-r5', '--record5', dest="record5", required=True, help="Record voice 5")
+    parser.add_argument('-r6', '--record6', dest="record6", required=True, help="Record voice 6")
+    parser.add_argument('-r7', '--record7', dest="record7", required=True, help="Record voice 7")
+    parser.add_argument('-r8', '--record8', dest="record8", required=True, help="Record voice 8")
+    parser.add_argument('-r9', '--record9', dest="record9", required=True, help="Record voice 9")
+    parser.add_argument('-r10', '--record10', dest="record10", required=True, help="Record voice 10")
     parser.add_argument('-n', '--name', dest="model_name", required=True, help="Personal model name")
     parser.add_argument('-lang', '--language', default="en", dest="language", help="Language")
     args = parser.parse_args()
@@ -42,7 +48,7 @@ def main():
     assert cut.BitsPerSample() == enroll.BitsPerSample()
     print("channels: %d, sample rate: %d, bits: %d" % (cut.NumChannels(), cut.SampleRate(), cut.BitsPerSample()))
 
-    recording_set = [args.record1, args.record2, args.record3, args.record4]
+    recording_set = [args.record1, args.record2, args.record3, args.record4, args.record5, args.record6, args.record7, args.record8, args.record9, args.record10]
     for rec in recording_set:
         print("processing %s" % rec)
         _, data = wavfile.read(rec)
